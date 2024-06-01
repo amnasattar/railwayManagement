b7cea310e6c9af516796d72e4700c83495599df3
Amna Sattar <amna123@email.com>
2024-06-01 15:26:35 +0500

    first commit

---
+++ Railway_project/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Railway_project.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Reservation()
+        {
+            return View();
+        }
+    }
+}
---
+++ Railway_project/Models/Reservation.cs
@@ -0,0 +1,11 @@
+﻿namespace Railway_project.Models
+{
+    public class Reservation
+    {
+
+        public int Id { get; set; }
+        public DateTime date { get; set; }
+        public int Seats { get; set; }
+
+    }
+}
